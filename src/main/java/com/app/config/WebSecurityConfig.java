9023246fe6646aab0f75e65c4aa2bbad3dc22f8f
WangShaoBin <wangshaobin@arrail-dental.com>
2019-06-19 12:10:10 +0800

    增加数据库支持

--- src/main/java/com/app/config/WebSecurityConfig.java
+++ src/main/java/com/app/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.app.config;
 
 import com.app.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Package com.app.config
@@ -20,18 +30,43 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  **/
 @Configuration
 @EnableWebSecurity // 注解开启Spring Security的功能
+@Slf4j
 //WebSecurityConfigurerAdapter:重写它的方法来设置一些web的安全西街
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()  //定义哪些url需要保护，哪些url不需要保护
-                .antMatchers("/", "/message/").permitAll()    //定义不需要认证就可以访问
+                .antMatchers("/", "/message/", "/css/**", "/static/**").permitAll()    //定义不需要认证就可以访问
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")  //定义当需要用户登录时候，转到的登录页面
-                .permitAll()
+                .permitAll().successHandler(new AuthenticationSuccessHandler() {
+            //登入成功后处理
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                log.info("登入成功了！！！！");
+                System.out.println("我要跳");
+
+                response.sendRedirect("/");
+            }
+        }).failureHandler(new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                log.info("登入失败了！！！！");
+
+                response.sendRedirect("/login?error");
+
+//                httpServletResponse.setContentType("application/json;charset=utf-8");
+//                PrintWriter out = httpServletResponse.getWriter();
+//                out.write("{\"status\":\"error\",\"message\":\"用户名或密码错误\"}");
+//                out.flush();
+//                out.close();
+
+            }
+        })
                 .and()
                 .logout()
                 .permitAll();
